6e0eb5bc1a6710e830ab1a342229a90054f95d37
Capucine <capucine.jumelle@cpe.fr>
2023-05-16 10:15:35 +0200

    cate

--- tp_courbure_Gaelle_version2/src/local/scene/scene.cpp
+++ tp_courbure_Gaelle_version2/src/local/scene/scene.cpp
@@ -46,10 +46,10 @@ using namespace cpe;
 void scene::build_surface_cylindrique()
 {
     // cylindrique forme;
-    // catenoide forme;
+    catenoide forme;
     // sphere forme;
     // para_hyper forme;
-    helicoide forme;
+    // helicoide forme;
     // pseudo_sphere forme;
     surface = forme.create(surface);
 
